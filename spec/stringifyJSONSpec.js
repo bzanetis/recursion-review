a8ed1238b4e8ddb3b10ede6480c06915abe6a16c
bzanetis <britt.zanetis19@gmail.com>
2019-01-02 19:22:49 -0600

    stringifyJSON progress/all tests passing

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -1,17 +1,24 @@
 // test cases are described in fixtures.js
+
+// var stringifiableObjects = expect("fixtures.js")
+// var unstringifiableValues = expect("fixtures.js")
+
 describe('stringifyJSON', function() {
   it('should match the result of calling JSON.stringify', function() {
+
+// console.log(stringifiableObjects);
 
     stringifiableObjects.forEach(function(test) {
       var expected = JSON.stringify(test);
       var result = stringifyJSON(test);
-      expect(result).to.equal(FILL_ME_IN);
+      expect(result).to.equal(expected);
+      console.log(stringifiableObjects);
     });
 
     unstringifiableValues.forEach(function(obj) {
       var expected = JSON.stringify(obj);
       var result = stringifyJSON(obj);
-      expect(result).to.equal(FILL_ME_IN);
+      expect(result).to.equal(expected);
     });
 
   });
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,53 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var resultArr = [];
+  if (typeof obj === 'function') {
+    return null;
+  } else if (typeof obj === 'number') {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'undefined') {
+    return null;
+  } else if (typeof obj === 'boolean') {
+    return '' + obj + '';
+  } else if (obj === null) {
+    return "null";
+
+  } else if (Array.isArray(obj)) {
+      for (var i = 0; i < obj.length; i++) {
+        resultArr.push(stringifyJSON(obj[i]))
+      }
+    return '[' + resultArr.join(',') + ']';
+  } else if (typeof obj === 'object') {
+      for (var key in obj) {
+        if (stringifyJSON(obj[key]) !== null) {
+          resultArr.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+        }
+      }
+    return '{' + resultArr.join(',') + '}';
+  }
 };
+
+
+
+
+//run through each type of input
+//checking typeof
+//numbers, boolean  =  return '' + obj
+//string = return '"' + obj + '"'
+
+//array (Array.isArray)
+  //create an empty []
+  //use for loop to look inside input arr
+    //push to result array -> use recursive call here
+  //return '[' + result array.join(',') + ']'
+//object
+ //use for in loop
+ //typeof && != undefined
+ // if statement ->  === undefined or typeof function, continue
+  //push key with stringifyJSON + ':' + obj[keys]
+ //return '{' + result array.join(',') + '}'
+
+//function, null
